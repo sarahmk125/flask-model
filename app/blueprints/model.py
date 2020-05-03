04cdb550ee91c1836f5bd3becdaa3aa308eba6a8
Sarah Krasnik <sarahmk125@gmail.com>
2020-05-03 14:35:53 -0400

    adding links; model parameter

--- app/blueprints/model.py
+++ app/blueprints/model.py
@@ -1,9 +1,9 @@
 from app import main
 from app import db
-from app.forms.model_forms import AddModelForm
+from app.forms.model_forms import AddModelForm, AddModelParameterForm
 from flask import redirect, url_for, render_template
 from flask_login import login_required
-from app.models.models import FinancialModel
+from app.models.models import FinancialModel, ModelParameter
 
 
 @main.route('/add-model', methods=['GET', 'POST'])
@@ -19,7 +19,7 @@ def add_model():
         financial_model = FinancialModel(
             name=form.name.data,
             version=form.version.data,
-            unique_name=form.unique_name.data
+            unique_name=form.name.data + '__' + form.version.data
         )
 
         # Save the model to the DB
@@ -29,6 +29,38 @@ def add_model():
         return redirect(url_for('main.add_model'))
 
     return render_template('add_model.html', form=form)
+
+
+@main.route('/add-model-parameter', methods=['GET', 'POST'])
+@login_required
+def add_model_parameter():
+    model_unique_name = None
+    unique_name = None
+    name = None
+    value = None
+    form = AddModelParameterForm()
+    if form.validate_on_submit():
+        # Need to make sure these are required?
+        # First, get the model id for the model unique name
+        # NOTE: Add error handling here
+        model_obj = FinancialModel.query.filter_by(unique_name=form.model_unique_name.data).first()
+        model_id = model_obj.id
+
+        # Construct the model parameters; unique name is model name + param name        
+        model_parameter = ModelParameter(
+            model_id = model_id,
+            name=form.name.data,
+            value=form.value.data,
+            unique_name=form.model_unique_name.data + '__' + form.name.data
+        )
+
+        # Save the model to the DB
+        db.session.add(model_parameter)
+        db.session.commit()
+
+        return redirect(url_for('main.add_model_parameter'))
+
+    return render_template('add_model_parameter.html', form=form)
 
 
 @main.route('/view-models', methods=['GET'])
--- app/forms/model_forms.py
+++ app/forms/model_forms.py
@@ -4,8 +4,15 @@ from wtforms.validators import DataRequired, Length
 
 
 class AddModelForm(FlaskForm):
-    unique_name = StringField('Unique model name (typically [name]_[version])', validators=[Length(0, 256)])
-    name = StringField('Model name', validators=[Length(0, 256)])
-    version = StringField('Version', validators=[Length(0, 8)])
+    name = StringField('Model name', validators=[Length(0, 256), DataRequired()])
+    version = StringField('Version', validators=[Length(0, 8), DataRequired()])
+    
+    submit = SubmitField('Submit')
+
+
+class AddModelParameterForm(FlaskForm):
+    model_unique_name = StringField('Unique model name (typically [name]__[version])', validators=[Length(0, 256), DataRequired()])
+    name = StringField('Parameter name', validators=[Length(0, 256), DataRequired()])
+    value = StringField('Parameter value (float)', validators=[DataRequired()])
 
     submit = SubmitField('Submit')
